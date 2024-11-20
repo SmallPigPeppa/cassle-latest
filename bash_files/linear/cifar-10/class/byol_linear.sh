b205aebfd4e9e5a6d9e107f280c0bb7b890e06b8
lwz <905328134@qq.com>
2024-11-20 22:01:05 +0800

    initial

--- bash_files/linear/cifar-10/class/byol_linear.sh
+++ bash_files/linear/cifar-10/class/byol_linear.sh
@@ -1,35 +1,5 @@
-#python3 main_linear.py \
-#    --dataset cifar10 \
-#    --encoder resnet18 \
-#    --data_dir $DATA_DIR \
-#    --split_strategy class \
-#    --num_tasks 1 \
-#    --max_epochs 100 \
-#    --gpus 0 \
-#    --precision 16 \
-#    --optimizer sgd \
-#    --scheduler step \
-#    --lr 3.0 \
-#    --lr_decay_steps 60 80 \
-#    --weight_decay 0 \
-#    --batch_size 256 \
-#    --num_workers 8 \
-#    --name byol-cifar10-5T-linear-eval \
-#    --pretrained_feature_extractor /ppio_net0/code/cassle-latest/ckpt/byol-cifar100-predictive-task0-ep=499-uidf2phh.ckpt \
-#    --entity pigpeppa \
-#    --project tnnls \
-#    --wandb \
-#    --save_checkpoint
-
-
-#byol-cifar100-predictive-task0-ep=499-uidf2phh.ckpt
-#byol-cifar10-32brzx9a-ep=999.ckpt
-
-
-
-
 python3 main_linear.py \
-    --dataset cifar100 \
+    --dataset cifar10 \
     --encoder resnet18 \
     --data_dir $DATA_DIR \
     --split_strategy class \
@@ -45,8 +15,38 @@ python3 main_linear.py \
     --batch_size 256 \
     --num_workers 8 \
     --name byol-cifar10-5T-linear-eval \
-    --pretrained_feature_extractor /ppio_net0/code/cassle-latest/ckpt/byol-cifar10-32brzx9a-ep=999.ckpt \
+    --pretrained_feature_extractor /ppio_net0/code/cassle-latest/ckpt/byol-cifar100-predictive-task1-ep=499-ptu1e3x1.ckpt \
     --entity pigpeppa \
     --project tnnls \
     --wandb \
     --save_checkpoint
+
+
+#byol-cifar100-predictive-task0-ep=499-uidf2phh.ckpt
+#byol-cifar10-32brzx9a-ep=999.ckpt
+#byol-cifar100-predictive-task1-ep=499-ptu1e3x1.ckpt
+
+
+
+#python3 main_linear.py \
+#    --dataset cifar100 \
+#    --encoder resnet18 \
+#    --data_dir $DATA_DIR \
+#    --split_strategy class \
+#    --num_tasks 1 \
+#    --max_epochs 100 \
+#    --gpus 0 \
+#    --precision 16 \
+#    --optimizer sgd \
+#    --scheduler step \
+#    --lr 3.0 \
+#    --lr_decay_steps 60 80 \
+#    --weight_decay 0 \
+#    --batch_size 256 \
+#    --num_workers 8 \
+#    --name byol-cifar100-5T-linear-eval \
+#    --pretrained_feature_extractor /ppio_net0/code/cassle-latest/ckpt/byol-cifar10-32brzx9a-ep=999.ckpt \
+#    --entity pigpeppa \
+#    --project tnnls \
+#    --wandb \
+#    --save_checkpoint
